72c86574ee874c0931b257549244670d9e860b4f
Oskar Månsson <o92design@gmail.com>
2022-01-17 17:21:47 +0100

    [CI] Changed Back to superunitybuild - one more time!! we're gonna celebrate!!

---
+++ TeamCityUnityBuild/Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,38 @@
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        Development,
+    }
+
+    public enum Platform
+    {
+        None,
+        WebGL,
+        PC,
+    }
+
+    public enum Architecture
+    {
+        None,
+        WebGL,
+        Windows_x64,
+    }
+
+    public enum Distribution
+    {
+        None,
+        itch,
+    }
+
+    public static readonly System.DateTime buildDate = new System.DateTime(637780366299789688);
+    public const string version = "0.0.0.1";
+    public const ReleaseType releaseType = ReleaseType.Development;
+    public const Platform platform = Platform.PC;
+    public const Architecture architecture = Architecture.Windows_x64;
+    public const Distribution distribution = Distribution.itch;
+}
+
